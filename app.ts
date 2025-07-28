0fee2210ffe0c1f27f39e3749f8a3fb93849f956
monika <monikakhanka@gmail.com>
2025-07-28 23:06:35 +0530

    Feathers server and socket

--- app.ts
+++ app.ts
@@ -1,4 +1,8 @@
 import { feathers } from "@feathersjs/feathers";
+import { koa, rest, bodyParser, serveStatic } from "@feathersjs/koa";
+
+import socketio from "@feathersjs/socketio";
+import { errorHandler } from "@feathersjs/koa";
 
 interface Message {
   id?: number;
@@ -27,28 +31,35 @@ type ServiceTypes = {
   messages: MessageService;
 };
 
-const app = feathers<ServiceTypes>();
+const app = koa<ServiceTypes>(feathers());
+
+// use the current folder for static file hosting
+app.use(serveStatic("."));
+
+// register the error handling
+app.use(errorHandler());
+
+// parse the JSON request body
+app.use(bodyParser());
+
+// register Rest service handler
+app.configure(rest());
+
+// configure socket.io in real time apis
+app.configure(socketio());
+
+// register our message service
 app.use("messages", new MessageService());
 
-app.service("messages").on("created", (message: Message) => {
-  console.log("A new message has been created", message);
-});
+// add any new realtime connection to the 'everybody' channel
+app.on("connection", (connection: any) =>
+  app.channel("everybody").join(connection)
+);
 
-const main = async () => {
-   await app.service("messages").create({
-    text: "Hello feathers",
-  });
+// publish all events to the 'everybody' channel
+app.publish((_data: any) => app.channel("everybody"));
 
-    
-  await app.service("messages").create({
-    text: "Hello again",
-  });
+// start the server
+app.listen(3030).then(() => console.log("Feathers server listening: 3030"));
 
-
-  const messages = app.service("messages").find();
-
-  console.log("All messages", messages);
-};
-
-main();
-
+app.service("messages").create({ text: "Hello world from the server" });
